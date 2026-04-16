9c1c9143ccd63e3c5895b03e8c7f880c2c603e8d
GodShiv888 <147086705+GodShiv888@users.noreply.github.com>
2026-04-17 03:56:03 +0530

    Update service.sh

--- template/service.sh
+++ template/service.sh
@@ -1,3 +1,6 @@
-DEBUG=@DEBUG@
-
-MODDIR=${0%/*}
+#!/system/bin/sh
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+    sleep 1
+done
+settings put system min_refresh_rate 120.0
+settings put system peak_refresh_rate 120.0
